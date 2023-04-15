e2757865cd0ecf65fe15de013820af8ce7c3302d
Himanshu <himanshu16315@gmail.com>
2023-04-15 19:42:16 -0400

    Added Splash Screen 🕸️

---
+++ app/src/main/java/com/finalproject/milestone_readbout/ui/activities/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.finalproject.milestone_readbout.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.finalproject.milestone_readbout.R;
+import com.finalproject.milestone_readbout.utils.Constants;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+                // close this activity
+
+                finish();
+            }
+
+        }, 2 * 1000); // wait for 5 seconds
+    }
+}
--- app/src/main/java/com/finalproject/milestone_readbout/utils/Constants.java
+++ app/src/main/java/com/finalproject/milestone_readbout/utils/Constants.java
@@ -14,7 +14,7 @@ public class Constants {
     public static final String LOGIN_FAILED = "Login Failed";
     public static final String REGISTRATION_FAILED = "Registration Failed";
     public static final String EMPTY_FIELDS_ERROR = "Fields cannot be empty";
-    public static final String NO_NEWS = "Sorry, no news exits!";
+    public static final String NO_NEWS = "Sorry, no news exists!";
     public static final String DATA_FETCHING_FAILED_FIREBASE = "Failed to fetch data from firebase";
     public static final String LOGOUT_ALERT = "Do you want to logout?";
     public static final String SEE_YOU_SOON = "See you soon!";
